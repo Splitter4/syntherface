f5760b5565b23bf2c19a597074cabac9851b59f7
Artur de Almeida Scheibler <artur.scheibler@gmail.com>
2017-09-20 21:01:10 -0300

    feat(MainActivity): Creating empty listeners for buttons.

--- android/Syntherface/app/src/main/java/arturscheibler/syntherface/MainActivity.java
+++ android/Syntherface/app/src/main/java/arturscheibler/syntherface/MainActivity.java
@@ -2,12 +2,52 @@ package arturscheibler.syntherface;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mConnectButton;
+    private Button mDisconnectButton;
+    private Button mSendButton;
+    private Button mClearButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mConnectButton = (Button)findViewById(R.id.connect_button);
+        mConnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mDisconnectButton = (Button)findViewById(R.id.disconnect_button);
+        mDisconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mSendButton = (Button)findViewById(R.id.send_button);
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mClearButton = (Button)findViewById(R.id.clear_button);
+        mClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
