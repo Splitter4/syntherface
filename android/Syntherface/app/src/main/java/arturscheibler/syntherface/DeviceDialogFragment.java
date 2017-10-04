25c7694cdd74ee2d3c308bdd184dc0ab35adbed6
Artur de Almeida Scheibler <artur.scheibler@gmail.com>
2017-10-04 20:40:26 -0300

    feat(DeviceDialogFragment): Feeding dialog with paired devices.

--- android/Syntherface/app/src/main/java/arturscheibler/syntherface/DeviceDialogFragment.java
+++ android/Syntherface/app/src/main/java/arturscheibler/syntherface/DeviceDialogFragment.java
@@ -3,36 +3,45 @@ package arturscheibler.syntherface;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeviceDialogFragment extends DialogFragment {
     /* The activity that creates an instance of this dialog fragment must
      * implement this interface in order to receive event callbacks.
      * Each method passes the DialogFragment in case the host needs to query it. */
     public interface DeviceDialogListener {
-        void onListItemClick(String s);
+        void onDeviceChosen(BluetoothDevice device);
     }
     
-    // Use this instance of the interface to deliver action events
-    DeviceDialogListener mListener;
-    ArrayAdapter<String> mAdapter;
+    DeviceDialogListener mListener; // Use this instance of the interface to deliver action events
+    ArrayAdapter<BluetoothDevice> mAdapter;
     
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
-        mAdapter.add("Hello");
-        mAdapter.add("Hi");
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        List<BluetoothDevice> devices = new ArrayList<>(bluetoothAdapter.getBondedDevices());
+        
+        mAdapter = new ArrayAdapter<>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                devices
+        );
         
         return new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.device_dialog_title)
                 .setAdapter(mAdapter, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        mListener.onListItemClick(mAdapter.getItem(which));
+                        mListener.onDeviceChosen(mAdapter.getItem(which));
                     }
                 })
                 .create();
