24a641c74515b40c13d5b4fb18203ae4d53cc13a
Artur de Almeida Scheibler <artur.scheibler@gmail.com>
2017-10-08 18:55:14 -0300

    feat(DeviceDialogFragment): Add discovered nearby devices to adapter.

--- android/Syntherface/app/src/main/java/arturscheibler/syntherface/DeviceDialogFragment.java
+++ android/Syntherface/app/src/main/java/arturscheibler/syntherface/DeviceDialogFragment.java
@@ -5,9 +5,13 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.widget.ArrayAdapter;
 
@@ -18,23 +22,42 @@ public class DeviceDialogFragment extends DialogFragment {
     /* The activity that creates an instance of this dialog fragment must
      * implement this interface in order to receive event callbacks.
      * Each method passes the DialogFragment in case the host needs to query it. */
-    public interface DeviceDialogListener {
+    interface DeviceDialogListener {
         void onDeviceChosen(BluetoothDevice device);
     }
     
-    DeviceDialogListener mListener; // Use this instance of the interface to deliver action events
-    ArrayAdapter<BluetoothDevice> mAdapter;
+    private DeviceDialogListener mListener; // Use this instance of the interface to deliver action events
+    private ArrayAdapter<BluetoothDevice> mAdapter;
     
-    @Override
+    @Override @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        List<BluetoothDevice> devices = new ArrayList<>(bluetoothAdapter.getBondedDevices());
+        final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        List<BluetoothDevice> pairedDevices = new ArrayList<>(bluetoothAdapter.getBondedDevices());
         
         mAdapter = new ArrayAdapter<>(
                 getActivity(),
                 android.R.layout.simple_list_item_1,
-                devices
+                pairedDevices
         );
+    
+        // Register for broadcasts when a device is discovered.
+        final BroadcastReceiver receiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    mAdapter.add(device);
+                } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                    bluetoothAdapter.cancelDiscovery();
+                }
+            }
+        };
+        
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        getActivity().registerReceiver(receiver, filter);
+        
+        bluetoothAdapter.startDiscovery();
         
         return new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.device_dialog_title)
