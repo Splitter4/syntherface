4c45483c8e46ae9519ba7265b0b541afa7c37f8c
Artur de Almeida Scheibler <artur.scheibler@gmail.com>
2017-10-25 22:44:33 -0200

    feat(MainActivity): Mocking use of the component list.

--- android/Syntherface/app/src/main/java/arturscheibler/syntherface/MainActivity.java
+++ android/Syntherface/app/src/main/java/arturscheibler/syntherface/MainActivity.java
@@ -1,13 +1,29 @@
 package arturscheibler.syntherface;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends FragmentActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("a");
+        strings.add("b");
+        strings.add("c");
+        strings.add("d");
+        strings.add("e");
+        
+        RecyclerView componentList = (RecyclerView) findViewById(R.id.component_list_rv);
+        componentList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        componentList.setAdapter(new ComponentAdapter(strings));
+        
     }
 }
