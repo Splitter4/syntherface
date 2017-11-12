b5b84d901aef2f8e94e912ea3c2abcb6cd61cf70
Artur de Almeida Scheibler <artur.scheibler@gmail.com>
2017-11-12 20:49:15 -0200

    refactor(Knob): Removing unused method.

--- android/Syntherface/app/src/main/java/arturscheibler/syntherface/Knob.java
+++ android/Syntherface/app/src/main/java/arturscheibler/syntherface/Knob.java
@@ -1,8 +1,5 @@
 package arturscheibler.syntherface;
 
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-
 class Knob extends SynthWidget {
     
     int getViewResourceId() {
@@ -12,9 +9,5 @@ class Knob extends SynthWidget {
     int getIconResourceId() {
         return R.mipmap.ic_knob;
     }
-    
-    void inflateAndAttachTo(ViewGroup root) {
-        LayoutInflater inflater = LayoutInflater.from(root.getContext());
-        inflater.inflate(R.layout.knob, root);
-    }
+
 }
