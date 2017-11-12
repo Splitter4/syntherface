ed28856c14891ebea8b92d5330fc8366db637377
Artur de Almeida Scheibler <artur.scheibler@gmail.com>
2017-11-12 17:23:19 -0200

    refactor(SynthWidget): Using own getters and setters inside class.

--- android/Syntherface/app/src/main/java/arturscheibler/syntherface/SynthWidget.java
+++ android/Syntherface/app/src/main/java/arturscheibler/syntherface/SynthWidget.java
@@ -12,7 +12,7 @@ abstract class SynthWidget {
     private RelativeLayout.LayoutParams mViewLayoutParams = null;
 
     void setShadowView(@NonNull View shadow) {
-        this.mShadow = shadow;
+        mShadow = shadow;
     }
 
     int getShadowSize() {
@@ -23,26 +23,40 @@ abstract class SynthWidget {
         return mView;
     }
 
-
+    private void setView(View view) {
+        mView = view;
+    }
+    
     RelativeLayout.LayoutParams getViewLayoutParams() {
         return mViewLayoutParams;
     }
     
+    private void setLayoutParams(RelativeLayout.LayoutParams layoutParams) {
+        mViewLayoutParams = layoutParams;
+        
+        View view = getView();
+        if (view != null) {
+            view.setLayoutParams(layoutParams);
+        }
+    }
+    
     void inflateFrom(@NonNull RelativeLayout root) {
         LayoutInflater inflater = LayoutInflater.from(root.getContext());
-        mView = inflater.inflate(getViewResourceId(), root, false);
-        mViewLayoutParams = (RelativeLayout.LayoutParams) mView.getLayoutParams();
+        View view = inflater.inflate(getViewResourceId(), root, false);
+        setView(view);
+        setLayoutParams((RelativeLayout.LayoutParams) view.getLayoutParams());
     }
 
     void deflate() {
-        mView = null;
-        mViewLayoutParams = null;
+        setView(null);
+        setLayoutParams(null);
     }
     
     void setPosition(int x, int y) {
-        mViewLayoutParams.leftMargin = x >= 0 ? x : 0;
-        mViewLayoutParams.topMargin = y >= 0 ? y : 0;
-        mView.setLayoutParams(mViewLayoutParams);
+        RelativeLayout.LayoutParams layoutParams = getViewLayoutParams();
+        layoutParams.leftMargin = x >= 0 ? x : 0;
+        layoutParams.topMargin = y >= 0 ? y : 0;
+        setLayoutParams(layoutParams);
     }
     
     abstract int getViewResourceId();
