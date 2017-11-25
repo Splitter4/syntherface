c9cea4afbd1bcd71505f0a94c0574f6143ffdcf0
Artur de Almeida Scheibler <artur.scheibler@gmail.com>
2017-11-25 13:38:02 -0200

    feat(Knob): Implementing getDialogLayoutResourceId() for Knob.

--- android/Syntherface/app/src/main/java/arturscheibler/syntherface/Knob.java
+++ android/Syntherface/app/src/main/java/arturscheibler/syntherface/Knob.java
@@ -2,12 +2,18 @@ package arturscheibler.syntherface;
 
 class Knob extends SynthWidget {
     
+    @Override
     int getViewResourceId() {
         return R.layout.knob;
     }
     
+    @Override
     int getIconResourceId() {
         return R.mipmap.ic_knob;
     }
 
+    @Override
+    int getDialogLayoutResourceId() {
+        return R.layout.dialog_knob;
+    }
 }
